82c0bb22d293612800d4d6b9f0762b49eb5c0401
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-06-15 12:38:41 +0200

    Fetch data from Pokedex API

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from "react";
+
+import Scoreboard from "./components/Scoreboard";
+
 function App() {
-  return <></>;
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const pokemonNames = [
+      "Pikachu",
+      "Bulbasaur",
+      "Charmander",
+      "Squirtle",
+      "Jigglypuff",
+      "Meowth",
+      "Psyduck",
+      "Snorlax",
+      "Eevee",
+      "Gengar",
+      "Machop",
+      "Mewtwo",
+    ];
+
+    const fetchPokemons = async () => {
+      const results = await Promise.all(
+        pokemonNames.map((name) =>
+          fetch(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`).then(
+            (res) => res.json()
+          )
+        )
+      );
+      setPokemons(results);
+    };
+
+    fetchPokemons();
+  }, []);
+
+  if (pokemons.length < 12) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <>
+      <Scoreboard score={score} bestScore={bestScore} />
+      <div className="pokemonGrid">
+        {pokemons.map((pokemon) => (
+          <div key={pokemon.id}>
+            <p>{pokemon.name}</p>
+            <img src={pokemon.sprites.front_default} alt={pokemon} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App;
